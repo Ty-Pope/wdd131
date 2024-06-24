f463efdaa3d30c3019628796b75011c0c8af8447
Ty-Pope <138350487+Ty-Pope@users.noreply.github.com>
2024-06-24 15:55:38 -0600

    forgot skip to main

--- recipes/main.js
+++ recipes/main.js
@@ -1,5 +1,6 @@
 import recipes from "./recipes.mjs";
 
+/*
 document.getElementById("main").innerHTML = recipes
  .map(
   (recipe) => `
@@ -15,7 +16,21 @@ document.getElementById("main").innerHTML = recipes
 `
  )
  .join("");
-
+*/
+let info = recipes[Math.floor(Math.random() * recipes.length)];
+document.getElementById("main").innerHTML = createTemplate(info);
+function createTemplate(recipe) {
+ return `
+  <div class="recipe">
+   <img class="recipe-img" src="${recipe.image}" alt="${recipe.name}" />
+   <div class="info">
+   <div class="tags">${getTags(recipe.tags)}</div>
+   <h2>${recipe.name}</h2>
+   <p class="star">${getStars(recipe.rating)}</p>
+   <p class="description">${recipe.description}</p>
+   </div>
+  </div>`;
+}
 function getTags(tags) {
  return tags.map((tag) => `<span class="tag">${tag}</span>`).join("");
 }
@@ -27,3 +42,11 @@ function getStars(rating) {
  }
  return stars + "</span>";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+ document.getElementById("submit-button").addEventListener("click", searchFilter(document.getElementById("search").value));
+
+ function searchFilter(inputValue) {
+  return recipes.filter((recipe) => recipe.name.toLowerCase().includes(inputValue.toLowerCase()));
+ }
+});
